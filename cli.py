f51937d46c53d4c634e949591905b7a379a78b2a
Tim Menzies <timm@ieee.org>
2021-02-17 14:05:31 -0500

    saving

--- cli.py
+++ cli.py
@@ -1,22 +1,43 @@
+#i!/usr/bin/env python3
+# vim: ts=2 sw=2 sts=2 et tw=81:
+"""
+Succinct CLI tool that uses from function name, docstring, and default args.   
+(c) 2021 Tim Menzies <timm@ieee.org>, MIT License
+
+Example usage:
+
+    from cli import cli
+    def ageAndShoeSize(
+            dob:    "date of birth" = 1960,
+            elated: "make happy"    = False,
+            where:  "birth place"   = ["nsw", "vic"],
+            shoes:  "shoesize"      = 10):
+      "Good times for all.  (c) Tim here now 2021"
+      print(f"{where} dob + shoes = {dob+shoes} elated= {elated}")
+    
+    if __name__ == "__main__": 
+      cli(ageAndShoeSize)
+
+"""
 import inspect
 import argparse as arg
 
 def cli(f):
   "Call `f`, first checking if any command line options override the defaults."
- #----------------------------------------------------
   do = arg.ArgumentParser(
           prog            = f.__name__,
           description     = (f.__doc__ or '').split("\n\n")[0],
           formatter_class = arg.RawDescriptionHelpFormatter)
   for key, v in inspect.signature(f).parameters.items():
     do.add_argument("-"+key, 
-                    **_details(v.default, v.annotation))
+                    **details(v.default, v.annotation))
   return f(**vars(do.parse_args())) 
 
-def _details(x,txt,choices=None):
+def details(x,txt,choices=None):
+  "Find the help text, defaults, return types withn `x`."
   isa= isinstance
   if isa(x, list): 
-    return _details(x[0], txt, choices=x)
+    return details(x[0], txt, choices=x)
   m, t = (("I", int)   if isa(x, int)   else (
           ("F", float) if isa(x, float) else ("S", str)))
   h = f"{txt}; default= {x} " + (f"range= {choices}" if choices else "")

c01173e55d4a40bf1eeea1cad8f1c3109d1d31f1
Tim Menzies <timm@ieee.org>
2021-02-17 20:12:39 -0500

    saving

--- xample.sh
+++
@@ -1,8 +0,0 @@
-out=${0%.*}.out
-cat<<EOF|while read -r x;do printf "\n#-------\n%% $x\n";$x;done|cat -n> $out
-python3 clink.py -h
-python3 clink.py
-python3 clink.py -w vic
-python3 clink.py -s 1000
-EOF
-cat $out
